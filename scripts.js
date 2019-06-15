05ac282b9ec39baf7de46c662e56bb008f48ef44
Anson VanDoren <anson.vandoren@gmail.com>
2019-06-15 12:53:48 -0700

    add different days support

--- scripts.js
+++ scripts.js
@@ -1,13 +1,36 @@
 Date.prototype.julian = function() {
   let today = new Date();
   // Start of this year (0th day)
-  const start = new Date(today.getFullYear(), 0, 0);
+  const start = new Date(this.getFullYear(), 0, 0);
   // without (possibly) daylight savings time
-  let diff = today - start;
+  let diff = this - start;
   // add in changes from daylight savings time if applicable
-  diff += (start.getTimezoneOffset() - today.getTimezoneOffset()) * 60 * 1000;
+  diff += (start.getTimezoneOffset() - this.getTimezoneOffset()) * 60 * 1000;
   // divide by milliseconds per day
   return Math.floor(diff / 86400000);
+};
+
+const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+const months = [
+  'Jan',
+  'Feb',
+  'Mar',
+  'Apr',
+  'May',
+  'Jun',
+  'Jul',
+  'Aug',
+  'Sep',
+  'Oct',
+  'Nov',
+  'Dec',
+];
+
+Date.prototype.getMonthName = function() {
+  return months[this.getMonth()];
+};
+Date.prototype.getDayName = function() {
+  return days[this.getDay()];
 };
 
 Number.prototype.nth = function() {
@@ -36,3 +59,85 @@ document.addEventListener(
   },
   false
 );
+
+const dayDisplay = document.getElementById('ordinal-day');
+const dayInput = document.getElementById('day-input');
+const dayPrefix = document.getElementById('day-prefix');
+const daySuffix = document.getElementById('day-suffix');
+
+document.getElementById('ordinal-day').addEventListener('click', (e) => {
+  const currentDay = parseInt(dayDisplay.innerHTML.slice(0, -2));
+
+  dayInput.value = currentDay;
+
+  updateWithDay();
+  showInput(true);
+});
+
+document.addEventListener('click', (e) => {
+  if (e.target.tagName === 'BODY') {
+    showInput(false);
+  }
+});
+
+function updateWithDay() {
+  // check if it's a leap year to get max days in the year
+  const currYear = new Date().getFullYear();
+
+  const daysThisYear = !(currYear & 3 || (!(currYear % 25) && currYear & 15))
+    ? 366
+    : 365;
+
+  const newDay = Math.min(daysThisYear, Math.max(parseInt(dayInput.value), 1));
+  if (isNaN(newDay)) {
+    dayInput.value = 1;
+    return;
+  }
+
+  let newDate = new Date(new Date().getFullYear(), 0, 0);
+  newDate.setDate(newDate.getDate() + newDay);
+
+  const todayDate = new Date().julian();
+  const otherDate = newDate.julian();
+  let tense;
+  if (otherDate < todayDate) {
+    tense = 'was';
+  } else if (otherDate > todayDate) {
+    tense = 'will be';
+  } else {
+    tense = 'is';
+  }
+
+  const displayDate = `${newDate.getDayName()}, ${newDate.getMonthName()} ${newDate
+    .getDate()
+    .nth()} <br/> ${tense} the `;
+
+  dayPrefix.innerHTML = displayDate;
+  daySuffix.innerHTML = `<span id='underlined'>${newDay
+    .nth()
+    .slice(-2)}</span><br/> day of the year`;
+
+  dayInput.value = newDay;
+  dayInput.style.width = `${String(newDay).length}ch`;
+}
+
+dayInput.addEventListener('keyup', () => {
+  updateWithDay();
+});
+
+document.addEventListener('keydown', (e) => {
+  if (e.key === 'Escape') {
+    showInput(false);
+  }
+});
+
+function showInput(shouldShow) {
+  dayDisplay.hidden = shouldShow;
+  dayInput.hidden = !shouldShow;
+  if (!shouldShow) {
+    dayPrefix.innerHTML = 'Today is the ';
+    daySuffix.innerHTML = ' day of the year';
+  } else {
+    dayInput.focus();
+  }
+}
